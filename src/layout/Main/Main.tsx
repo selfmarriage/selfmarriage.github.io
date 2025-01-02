b644cbeda96c899a3cb0adf88d4aa77a864d5904
selfmarriage <itselfdoit@gmail.com>
2025-01-03 04:58:48 +0900

    Update Main.tsx

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
-import mainImg from '@/assets/images/IMG5438-banner.jpg'
+import mainImg from '@/assets/images/01.jpg'
 
 const Main = () => {
   const { greeting } = data;
